d984def54837178db2ec409edaf14bc34c101c5a
toryoto <s1f102002345@iniad.org>
2024-07-15 14:20:15 +0900

    Echoスマートコントラクタのテスト

--- contract/scripts/run.ts
+++ contract/scripts/run.ts
@@ -1,11 +1,27 @@
 import { ethers } from "hardhat";
 
 const main = async () => {
+  const [owner, randomPerson] = await ethers.getSigners();
   const echoContractFactory = await ethers.getContractFactory('EthEcho');
   const echoContract = await echoContractFactory.deploy();
   const EthEcho = await echoContract.waitForDeployment();
+
   const deployedContractAddress = await echoContract.getAddress();
-  console.log("Contract added to:", deployedContractAddress);
+  console.log("Contract deployed to:", deployedContractAddress);
+  console.log("Contract deployed by:", owner.address);
+
+  await echoContract.getTotalEchoes();
+
+  let echoTxn = await echoContract.writeEcho();
+  // ユーザがウォレットでトランザクションを承認するのを待つ
+  await echoTxn.wait();
+
+  await echoContract.getTotalEchoes();
+
+  // 他のユーザがオーナーにEchoを送ったシュミレーション
+  echoTxn = await echoContract.connect(randomPerson).writeEcho();
+  await echoTxn.wait();
+  await echoContract.getTotalEchoes();
 };
 
 const runMain = async () => {
