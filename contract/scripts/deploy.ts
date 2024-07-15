82808b7d743c602e7557cce4cc0e39fd9ccff57d
toryoto <s1f102002345@iniad.org>
2024-07-16 00:37:37 +0900

    テストネットワークデプロイ

---
+++ contract/scripts/deploy.ts
@@ -0,0 +1,28 @@
+import { ethers } from "hardhat";
+
+const main = async () => {
+  const [deployer] = await ethers.getSigners();
+  // デプロイアカウントの残高(wei)
+  const accountBalance = await deployer.provider.getBalance(deployer.address);
+  const echoContractFactory = await ethers.getContractFactory("EthEcho");
+  const echoContract = await echoContractFactory.deploy();
+  const ethEcho = await echoContract.waitForDeployment();
+
+  console.log("Deploying contracts with account: ", deployer.address);
+  console.log("Account balance: ", accountBalance.toString());
+  const deployedContractAddress = await ethEcho.getAddress();
+  console.log("Contract deployed to:", deployedContractAddress);
+  console.log("Contract deployed by: ", deployer.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
