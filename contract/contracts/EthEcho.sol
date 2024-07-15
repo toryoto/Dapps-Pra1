44a39f7f2ab9626ded280b32512c019097ec9af5
toryoto <s1f102002345@iniad.org>
2024-07-15 14:20:01 +0900

    チェーン上のEchoを増やし、総数を取得するスマートコントラクタ

--- contract/contracts/EthEcho.sol
+++ contract/contracts/EthEcho.sol
@@ -5,7 +5,20 @@ pragma solidity ^0.8.19;
 import "hardhat/console.sol";
 
 contract EthEcho {
+  uint256 private _totalEchoes;
+
   constructor() {
     console.log("Here is my first smart contract");
+  }
+
+  function writeEcho() public {
+    _totalEchoes += 1;
+    console.log("%s has echoed!", msg.sender);
+  }
+
+  // viewでは読み取りのみなので、ガス代が表示されない
+  function getTotalEchoes() public view returns (uint256) {
+    console.log("We have %d total echoes!", _totalEchoes);
+    return _totalEchoes;
   }
 }
