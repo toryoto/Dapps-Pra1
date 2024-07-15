dfb4e6646019d870452a6058b54983300691b152
toryoto <s1f102002345@iniad.org>
2024-07-16 01:58:40 +0900

    envの追加

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -2,7 +2,13 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: "0.8.19",
+  // networks: {
+  //   sepolia: {
+  //     url: process.env.ALCHEMY_API_URL ?? "",
+  //     accounts: [process.env.PRIVATE_SEPOLIA_ACCOUNT_KEY ?? ""],
+  //   },
+  // },
 };
 
 export default config;
