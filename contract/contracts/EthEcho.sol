3b2a0ce7a2ac1f8005f7c11224099b5a8434991c
toryoto <s1f102002345@iniad.org>
2024-07-15 13:28:44 +0900

    first commit

---
+++ contract/contracts/EthEcho.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import "hardhat/console.sol";
+
+contract EthEcho {
+  constructor() {
+    console.log("Here is my first smart contract");
+  }
+}
