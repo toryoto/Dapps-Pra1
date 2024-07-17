c381e14256d3a07d2288a4c1e4ceb5b6be302947
toryoto <s1f102002345@iniad.org>
2024-07-17 13:27:39 +0900

    テストコードの記述

--- contract/scripts/run.ts
+++ contract/scripts/run.ts
@@ -1,27 +1,27 @@
 import { ethers } from "hardhat";
 
 const main = async () => {
-  const [owner, randomPerson] = await ethers.getSigners();
+  // const [owner, randomPerson] = await ethers.getSigners();
   const echoContractFactory = await ethers.getContractFactory('EthEcho');
   const echoContract = await echoContractFactory.deploy();
-  const EthEcho = await echoContract.waitForDeployment();
+  // const EthEcho = await echoContract.waitForDeployment();
 
   const deployedContractAddress = await echoContract.getAddress();
-  console.log("Contract deployed to:", deployedContractAddress);
-  console.log("Contract deployed by:", owner.address);
+  console.log("Contract added to: ", deployedContractAddress);
 
-  await echoContract.getTotalEchoes();
-
-  let echoTxn = await echoContract.writeEcho();
+  let echoTxn = await echoContract.writeEcho("A first message!");
+  let latestEcho = await echoContract.getLatestEcho();
   // ユーザがウォレットでトランザクションを承認するのを待つ
   await echoTxn.wait();
+  console.log(latestEcho);
 
-  await echoContract.getTotalEchoes();
-
-  // 他のユーザがオーナーにEchoを送ったシュミレーション
-  echoTxn = await echoContract.connect(randomPerson).writeEcho();
+  // 別のユーザでEchoを呼び出す
+  const [_, randomPerson] = await ethers.getSigners();
+  echoTxn = await echoContract.connect(randomPerson).writeEcho("Another message!");
   await echoTxn.wait();
-  await echoContract.getTotalEchoes();
+
+  latestEcho = await echoContract.getLatestEcho();
+  console.log(latestEcho);
 };
 
 const runMain = async () => {
