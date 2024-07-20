5945f32dfc96f4334be6350ca3f5d8980e5d5cdd
toryoto <s1f102002345@iniad.org>
2024-07-21 02:32:10 +0900

    スマートコントラクタに勉強用コメンと追加

--- contract/contracts/EthEcho.sol
+++ contract/contracts/EthEcho.sol
@@ -7,6 +7,8 @@ import "hardhat/console.sol";
 contract EthEcho {
     uint256 private _totalEchoes;
 
+    // eventが発火するとブロックチェーン上にデータを保存する
+    // フロントエンドは、このイベントをリッスンすることで、リアルタイムでUIを更新できる
     event NewEcho(address indexed from, uint256 indexed echoId, uint256 timestamp, string cid);
     event DeleteEcho(uint256 indexed echoId, address indexed from, uint256 timestamp);
 
@@ -17,6 +19,7 @@ contract EthEcho {
         uint256 timestamp;
     }
 
+    // idからEchoを取得するためのマッピング
     mapping(uint256 => Echo) private _echoesMap;
     // 消去されていないEchoのIDを格納するリスト
     uint256[] private _activeEchoIds;
@@ -35,7 +38,9 @@ contract EthEcho {
         console.log("%s has echoed with CID: ", msg.sender, _cid);
 
         Echo memory newEcho = Echo(_totalEchoes, msg.sender, _cid, block.timestamp);
+        // _echoesMapの最後に新しいエコーを追加
         _echoesMap[_totalEchoes] = newEcho;
+        // アクティブなエコー配列にintのidを追加
         _activeEchoIds.push(_totalEchoes);
 
         emit NewEcho(msg.sender, _totalEchoes, block.timestamp, _cid);
@@ -45,6 +50,7 @@ contract EthEcho {
         require(_echoesMap[_echoId].echoer == msg.sender, "Only the sender can remove their echo");
 
         // スワップ・アンド・ポップで削除対象を_activeEchoIdsから消去
+        // 配列を詰める作業をなくして、ガス代を節約する
         for (uint256 i = 0; i < _activeEchoIds.length; i++) {
             if (_activeEchoIds[i] == _echoId) {
                 // 削除対象の位置にリストの最後のデータを格納する
@@ -54,8 +60,11 @@ contract EthEcho {
             }
         }
 
+        // 変数を初期値にリセットするために使用
+        // _echoIdのEcho構造体が空になる→削除されてるように見える
         delete _echoesMap[_echoId];
 
+        // エコーが削除されたことをブロックチェーン上に記録
         emit DeleteEcho(_echoId, msg.sender, block.timestamp);
     }
 
