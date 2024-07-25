74f9ed5e76f1caf45b6caa5a79c811dafaf2d066
toryoto <s1f102002345@iniad.org>
2024-07-25 15:52:05 +0900

    ブロックチェーン上からCIDを取得して表示する

--- contract/front/src/app/api/pinata/profile/[address]/route.ts
+++ contract/front/src/app/api/pinata/profile/[address]/route.ts
@@ -1,14 +1,32 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getProfileDetailsFromPinata } from '../../pinataUtils';
+import { getDetailsCIDFromBlockchain, getNameFromBlockChain } from '@/utils/profileContract';
+import { N } from 'ethers';
+
+interface ProfileDetails {
+  bio: string;
+  imageHash?: string;
+}
 
 export async function GET(
   request: NextRequest,
   { params }: { params: { address: string } }
 ) {
   try {
-    const profile = await getProfileDetailsFromPinata(params.address);
-    if (profile) return NextResponse.json(profile);
-    else return NextResponse.json({ error: 'Profile not found' }, { status: 404 });
+    const name = await getNameFromBlockChain(params.address);
+    const detailsCID = await getDetailsCIDFromBlockchain(params.address);
+    if (!name || !detailsCID)  return NextResponse.json({ error: 'Profile not found on blockchain' }, { status: 404 });
+
+    const profileDetails: ProfileDetails | null = await getProfileDetailsFromPinata(detailsCID);
+    if (!profileDetails) return NextResponse.json({ error: 'Profile details not found on IPFS' }, { status: 404 });
+
+    const profile = {
+      name,
+      bio: profileDetails.bio,
+      imageCID: profileDetails.imageHash || ''
+    };
+
+    return NextResponse.json(profile)
   } catch (error) {
     console.error('Error in get profile API:', error);
     return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
