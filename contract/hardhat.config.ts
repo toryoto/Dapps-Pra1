1a974ba2df713a1a706909519c1eae2c81a36655
toryoto <s1f102002345@iniad.org>
2024-07-27 03:16:30 +0900

    local networkデプロイを可能にする

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -5,6 +5,9 @@ require("dotenv").config();
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
     sepolia: {
       url: process.env.ALCHEMY_API_URL ?? "",
       accounts: [process.env.PRIVATE_SEPOLIA_ACCOUNT_KEY ?? ""],
