e9fa2f687716f00728ce7013f2a2fe7a0212f07e
toryoto <s1f102002345@iniad.org>
2024-07-30 20:49:06 +0900

    ReadOnlyContractを使用してすべてのEchoを取得するAPI

---
+++ contract/front/src/app/api/echoes/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+import { ethers } from "ethers";
+import { getMessageFromPinata } from "../pinata/pinataUtils";
+import abi from "../../utils/EthEcho.json";
+import { RawEcho } from "@/app/types/type";
+
+const contractAddress = "0x3Cd556A69C4908Cd1034d29c10D6250E712F1EB3";
+const contractABI = abi.abi;
+
+// 読み取り専用のコントラクトを使用してすべてのEchoを取得する処理
+export async function GET(request: Request) {
+  try {
+    // JsonRpcProvider を使用して、読み取り専用のプロバイダーを作成
+    const provider = new ethers.JsonRpcProvider(process.env.ALCHEMY_API_URL);
+    const contract = new ethers.Contract(contractAddress, contractABI, provider);
+
+    // スマートコントラクトからすべてのエコーのCIDを取得
+    // struct Echo {
+    //   uint256 id;
+    //   address echoer;
+    //   string cid;
+    //   uint256 timestamp;
+    // }
+    const echoes = await contract.getAllEchoes();
+
+    const processedEchoes = await Promise.all(echoes.map(async (echo: RawEcho) => {
+      const message = await getMessageFromPinata(echo.cid)
+      
+      return {
+        id: Number(echo.id),
+        address: echo.echoer,
+        timestamp: new Date(Number(echo.timestamp) * 1000).toLocaleDateString('sv-SE'),
+        cid: echo.cid,
+        message: message,
+      };
+    }));
+
+    return NextResponse.json(processedEchoes)
+  } catch (error) {
+    console.error("Failed to create read-only contract instance:", error);
+    return null;
+  }
+}
